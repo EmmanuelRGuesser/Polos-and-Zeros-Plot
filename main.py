1c83076b075bd1d4ec6dc897be334dce284e4feb
EmmanuelRG <88853327+EmmanuelRGuesser@users.noreply.github.com>
2022-11-17 21:08:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# Este código cria uma grafico de polos e zeros dado uma funcao de transferencia,
-# sao gerados um grafico 2D onde mostra os polos e zeros, um em 3D que mostra a
-# magnetude, e o outro mostrando o corte do anterior no eixo real igual a zero.
+# Este código cria uma gráfico de polos e zeros dado uma função de transferência,
+# sao gerados um gráfico 2D onde mostra os polos e zeros, um em 3D que mostra a
+# magnitude, e o outro mostrando o corte do anterior no eixo real igual a zero.
 
 import control as ct
 import matplotlib.pyplot as plt
@@ -8,40 +8,41 @@ import numpy as np
 
 # Funcao de Tranferencia a ser computada
 def f(s):
-    return ((s + 1)*(s + 2))/((s**2 + 4*s + 5)*(s + 3))
+    return ((s + 1)*(s + 2))/((s**2 + 2*s + 3)*(s + 3))
 
 # Plota o Grafico de polos e zeros
 s = ct.TransferFunction.s
 k = ct.pzmap(f(s), plot=True, title = 'Diagrama de Polos e Zeros')
 
 # Determina o intervalo do eixo real e imag para o plotar a magnetude da funcao
-x = np.linspace(min(min(k[0].real),min(k[1].real)) - abs(min(min(k[0].real),min(k[1].real)))*.5,
-                max(max(k[0].real),max(k[1].real)) + abs(max(max(k[0].real),max(k[1].real)))*.5,
+x = np.linspace(min(min(k[0].real),min(k[1].real),min(k[0].imag),min(k[1].imag)) -
+                abs(min(min(k[0].real),min(k[1].real), min(k[0].imag),min(k[1].imag))),
+                
+                max(max(k[0].real),max(k[1].real),max(k[0].imag),max(k[1].imag)) + 
+                abs(max(max(k[0].real),max(k[1].real), max(k[0].imag),max(k[1].imag))),
+                
                 100)
-y = np.linspace(min(min(k[0].imag),min(k[1].imag)) - abs(min(min(k[0].imag),min(k[1].imag)))*.5,
-                max(max(k[0].imag),max(k[1].imag)) + abs(max(max(k[0].imag),max(k[1].imag)))*.5,
-                100)
-X, Y = np.meshgrid(x, y)
+
+X, Y = np.meshgrid(x, x)
 
 # Calcula a magnetude
 G = abs(f(X + Y*1j))
 
-# Plot grafico 3D da magnitude
-fig = plt.figure()
+# Configuracao do grafico da magnitude
+plt.figure()
 ax = plt.axes(projection='3d')
 ax.set_zlim([0,5])
 ax.plot_surface(X, Y, G, cmap='coolwarm')
-ax.set_title('Magnetude da Funcao de Tranferencia')
+ax.set_title('Magnitude da Funcao de Tranferencia')
 ax.set_xlabel('Real')
 ax.set_ylabel('Imaginario')
-ax.set_zlabel('Magnetude')
+ax.set_zlabel('Magnitude')
 
-# Plot do grafico 2D da magnitude em corte em real=0
+# Plot do eixo imag
 plt.figure()
-plt.plot(y, f(y*1j))
+plt.plot(x, f(x*1j))
 plt.xlabel('Imaginario')
-plt.ylabel('Magnetude')
+plt.ylabel('Magnitude')
 plt.grid(True)
 
-# Exibe os graficos
 plt.show()
